d299c22bd8b2fb993584a5d5751333da077c9584
MarinaRH <l_marina1717@hotmail.com>
2018-05-16 22:40:58 -0500

    adding component Cards

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Cards from './components/Cards.js';
+
+class App extends Component {
+  
+  render() {
+    return(
+      <div>
+        <Cards />
+      </div>
+    )}
+}
+
+
+
+export default App;
